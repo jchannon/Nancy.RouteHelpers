4bdddec5ecdca43b0b8ff9f810c9a13bdec9fb2a
Jonathan Channon <jonathan.channon@gmail.com>
2012-04-14 13:34:57 +0100

    Added more methods with explanation and examples

--- Nancy.RouteHelpers/RouteParameters.cs
+++ Nancy.RouteHelpers/RouteParameters.cs
@@ -5,13 +5,37 @@ namespace Nancy.RouteHelpers
 	{
 		public static string AnyInt()
 		{
-			return @"/(?<id>[\d]+)";
+			return @"/(?<id>[\d]*)";
 		}
 
 		public static string AnyInt(int lengthStart, int lengthEnd)
 		{
 			return @"/(?<id>[\d]{" + lengthStart + "," + lengthEnd + "})";
 		}
+
+        /// <summary>
+        /// Creates a route that will expect one or more ints
+        /// </summary>
+        /// <example>
+        /// /products/1 or /products/123
+        /// </example>
+        /// <returns></returns>
+        public static string AnyIntAtLeastOnce()
+        {
+            return @"/(?<id>[\d]+)";
+        }
+
+        /// <summary>
+        /// Creates a route that will accept an int but doesn't have to
+        /// </summary>
+        /// <example>
+        /// /products/1 or /products
+        /// </example>
+        /// <returns></returns>
+        public static string PossibleInt()
+        {
+            return @"/(?<id>[\d]*)";
+        }
 
         public static string And()
         {
