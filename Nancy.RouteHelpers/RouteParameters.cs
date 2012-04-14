299683d2953008d72d1c4339217fd147baa29659
Johan Nilsson <jonh04@gmail.com>
2012-04-14 01:32:55 -0400

    Renamed AnyInt parameters to lengthStart and lengthEnd

--- Nancy.RouteHelpers/RouteParameters.cs
+++ Nancy.RouteHelpers/RouteParameters.cs
@@ -8,9 +8,9 @@ namespace Nancy.RouteHelpers
 			return @"/(?<id>[\d]+)";
 		}
 
-		public static string AnyInt(int val1, int val2)
+		public static string AnyInt(int lengthStart, int lengthEnd)
 		{
-			return @"/(?<id>[\d]{" + val1 + "," + val2 + "})";
+			return @"/(?<id>[\d]{" + lengthStart + "," + lengthEnd + "})";
 		}
 
 		//string, string length, certain string, certain number
